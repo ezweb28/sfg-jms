0da35325722d713467047a4dfbabfbd59c3f2cc8
Sunpil Kim <ezweb28@gmail.com>
2022-08-14 00:15:57 -0400

    CH 453. Java Message Object Creation

---
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgjms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = 1096500110836666751L;
+
+    private UUID id;
+    private String message;
+}
